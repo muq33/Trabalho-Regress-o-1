c355cded34686c8e4c435a5112d41825596a8caf
muq33 <muqq33@gmail.com>
2023-07-08 15:37:23 -0300

    Update script.R

--- script.R
+++ script.R
@@ -46,9 +46,22 @@ ggplot(dados, aes(x=Gestation, y=LifeSpan)) + geom_point() + xlab(covar2$nome_us
 cor(dados[,covar1$nome_dados], dados[,var_resp$nome_dados], use = "complete.obs")
 cor(dados[,covar2$nome_dados], dados[,var_resp$nome_dados], use = "complete.obs")
 
+#Dividindo os dados em 50/50
+tamanho_total <- nrow(dados)
+tamanho_treino <- floor(tamanho_total/2)
+
+ind_treino <- sample(tamanho_total, tamanho_treino, replace = F)
+ind_teste <- setdiff(seq_len(tamanho_total), ind_treino)
+
+treino <- dados[ind_treino,]
+teste <- dados[ind_teste,]
+
+
 #Ajuste dos modelos
-ajuste_cov1_resp <- lm(dados[, var_resp$nome_dados]~dados[,covar1$nome_dados])
+ajuste_cov1_resp <- lm(treino[, var_resp$nome_dados]~treino[,covar1$nome_dados])
 summary(ajuste_cov1_resp)
-ajuste_cov2_resp <- lm(dados[, var_resp$nome_dados]~dados[,covar2$nome_dados])
+ajuste_cov2_resp <- lm(treino[, var_resp$nome_dados]~treino[,covar2$nome_dados])
 summary(ajuste_cov2_resp)
+
+#
 
